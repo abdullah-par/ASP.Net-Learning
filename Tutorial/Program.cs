8aa3d4694ed007f6c4611a0c59e58a8784e9f5c0
Abdullah Parvez <abdullahparvez43@gmail.com>
2026-03-03 01:34:57 +0530

    comments-are-added

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -23,7 +23,7 @@ app.Run(async (HttpContext context) =>
             var students =  StudentsRepository.GetStudents();
             foreach(var student in students)
             {
-                await context.Response.WriteAsync($"{student.Name} enrolled in {student.Branch}\r\n");
+                await context.Response.WriteAsync($"{student.Name} enrolled in {student.Branch} \r\n");
             }
         }
         
@@ -33,19 +33,23 @@ app.Run(async (HttpContext context) =>
 
 app.Run();// runs the application and starts listening for incoming HTTP requests.(starts the kestral server)
 
+//This is Respository Pattern, which is a design pattern that provides a way to manage data access and storage in an application. It abstracts the data layer and provides a simple interface for accessing and manipulating data. In this case, the StudentsRepository class serves as a repository for managing student data. It provides a method GetStudents() that returns a list of students, allowing other parts of the application to interact with the student data without needing to know the details of how it is stored or accessed.
 static class StudentsRepository
 {
+    //static because we are using it as a simple data storage. (Repository Pattern)
     private static List<Student> students = new List<Student>
+    //these curly braces are used to initialize the list with some predefined student objects. This is called collection initializer syntax in C#. It allows us to create and populate the list in a concise way. Each new Student object is created with an Id, Name, and Branch, and added to the students list. They are called Collection Initializer Syntax
     {
         new Student (1, "Abdullah", "AIML"),
         new Student (2, "Abdul", "AIML"),
-        new Student (2, "Rabiul", "AIML")
+        new Student (3, "Rabiul", "AIML")
     };
+    // => is called an expression-bodied member. It is a concise way to define a method that consists of a single expression. In this case, the GetStudents method returns the students list directly without needing a block of code. 
     public static List<Student> GetStudents() => students;
 }
 class Student
 {
-    public int Id { get; set; }
+    public int Id { get; set; } //auto property for Id
     public String Name { get; set; }
     public String Branch { get; set; }
     public Student(int id, string name, string branch)
